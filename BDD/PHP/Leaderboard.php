4f0c4b2907514aed1fb706968c8d148129f642e3
jil3401 <jil3401@gmail.com>
2022-11-05 22:43:29 +0100

    modif mineur

--- BDD/PHP/Leaderboard.php
+++ BDD/PHP/Leaderboard.php
@@ -1,3 +1,8 @@
 <?php 
-    echo 'yeah';
+    include('connect.php');
+    $results = mysqli_query($link, "SHOW TABLES FROM escapegame LIKE 'nom_joueur'");
+    if (isset($results)){
+        mysqli_query($link,'CREATE TABLE nom_joueur');
+    };
+    echo isset($results);
 ?>
