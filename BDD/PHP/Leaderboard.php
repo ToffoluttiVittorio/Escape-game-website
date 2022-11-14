14a1805527040c461f5bac0beb0b2ddd4b20a424
jil3401 <jil3401@gmail.com>
2022-11-14 23:01:59 +0100

    changement du nom de variable

--- BDD/PHP/Leaderboard.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include('connect.php');
-    $results = mysqli_query($link, "SHOW TABLES FROM escapegame LIKE 'nom_joueur'");
-    if (isset($results)){
-        mysqli_query($link,'CREATE TABLE nom_joueur');
-    };
-    echo isset($results);
-?>
