f6411cc6200b6c6040aa0fe09ab937bc289238fe
jil3401 <jil3401@gmail.com>
2022-11-19 18:03:57 +0100

    renommé en script_page_accueil

--- BDD/JS/script1.js
+++
@@ -1,8 +0,0 @@
-let joueur=document.getElementById('joueur');
-let nom=document.getElementById('nom');
-joueur.addEventListener('click',function(event){
-    event.preventDefault();});
-nom.addEventListener('input',function(event){
-    console.log(event);
-    console.log(nom.value);
-});
