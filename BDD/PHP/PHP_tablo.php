79d07905f8bb0036c434aa76fa4d16d192eb959b
jil3401 <jil3401@gmail.com>
2022-11-21 21:15:10 +0100

    simplification (en travail)

--- BDD/PHP/PHP_tablo.php
+++ BDD/PHP/PHP_tablo.php
@@ -1,19 +1,10 @@
 <?php
     include('connect.php');
+    $tableau = [];
     $tablo_joueur=mysqli_query($link,'SELECT * FROM escapegame ORDER BY score DESC LIMIT 10');
-    echo $tablo_joueur;
-    echo "<table>";
-            echo '<tr>';
-            echo "<td>joueur</td>";
-            foreach($tablo_joueur[0] as $key => $elem){
-                echo "<td>".$key."</td>";
-            }
-            echo "</tr>";
-            echo '<tr>';
-            echo "<td>score</td>";
-            foreach($tablo_joueur[1] as $key => $elem){
-                echo "<td>".$key."</td>";
-            }
-            echo "</tr>";
-            echo "</table>";
+    while($ligne = $tablo_joueur{
+        $tableau[] = $ligne;
+      }
+    echo json_encode($tableau);
+    //code destinée à afficher les scores des joueurs dans une page PHP simple, afin de mettre en ligne dans les 
 ?>
