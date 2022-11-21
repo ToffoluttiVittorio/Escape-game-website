b4f3c3c6a4d039838b23939b8358713278583947
jil3401 <jil3401@gmail.com>
2022-11-22 00:23:38 +0100

    Mise en place de la nouvelle architecture Test de Leaflet pour comprendre dans quoi je mettais les pieds pour les requêtes fetch et pour le json, je met en place le fichier pour les requêtes

--- BDD/JS/leaflets.js
+++ BDD/JS/leaflets.js
@@ -1,5 +1,44 @@
-var map = L.map('map').setView([0,0], 13);
+var map = L.map('map').setView([ -104.99404, 39.75621], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-	maxZoom: 20,
+	maxZoom: 19,
 	attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+// L.geoJSON(geojsonFeature).addTo(map);
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+L.geoJSON(geojsonFeature, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).bindPopup(function (layer) {
+    return layer.feature.properties.popupContent;
+}).addTo(map);
+
+// L.geoJSON(geojsonFeature, {
+//     onEachFeature: onEachFeature
+// }).addTo(map);
