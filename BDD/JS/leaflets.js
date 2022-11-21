27bfe3e3478adecff0c5682daa668beaa5fdb769
jil3401 <jil3401@gmail.com>
2022-11-21 21:13:20 +0100

    test leaflet pour le dev de nvelles fonctions (basées sur le fetch)

---
+++ BDD/JS/leaflets.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([0,0], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+	maxZoom: 20,
+	attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
