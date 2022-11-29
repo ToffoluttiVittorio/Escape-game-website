438ae097bb31282209d5a6a510767e9cadbe84eb
jil3401 <jil3401@gmail.com>
2022-11-29 19:27:31 +0100

    oui

--- BDD/JS/leaflets.js
+++ BDD/JS/leaflets.js
@@ -1,44 +1,16 @@
-var map = L.map('map').setView([ -104.99404, 39.75621], 13);
+var map = L.map('map').setView([48.245, 2.0345], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-	maxZoom: 19,
+	maxZoom: 20,
 	attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
-// L.geoJSON(geojsonFeature).addTo(map);
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
-}
-
-L.geoJSON(geojsonFeature, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions);
-    }
-}).bindPopup(function (layer) {
-    return layer.feature.properties.popupContent;
-}).addTo(map);
-
-// L.geoJSON(geojsonFeature, {
-//     onEachFeature: onEachFeature
-// }).addTo(map);
+//nous réalisons un fetch de la fonction  
+var data;
+fetch('../PHP/json_point_request.php', {
+    method: 'post',
+    body: data
+  })
+	.then(r => r.json())
+	.then(r=> data=JSON.parse(r))
+	.then(r => {console.log(r)})
+	.catch(console.error);
+console.log(data);
--- BDD/PHP/json_point_request.php
+++ BDD/PHP/json_point_request.php
@@ -1,3 +1,18 @@
 <?php
-
+    include('connect.php');
+    $request1=mysqli_query($link, "SELECT ob.id_point, po.lat, po.long FROM objet AS ob JOIN point AS po ON ob.id_point=po.id_point WHERE ob.id_point = 1");
+    $AE="SELECT ob.id_point, po.lat, po.long FROM objet AS ob JOIN point AS po ON ob.id_point=po.id_point WHERE ob.id_point=";
+    // echo $request1;
+    $request2=mysqli_query($link, "SELECT * FROM objet");
+    if($result = $request2){
+        $tableau=[];
+        while($ligne1 = mysqli_fetch_assoc($result)){
+            $interm=$ligne1["id_point"];
+            $ligne2 = mysqli_fetch_assoc(mysqli_query($link, $AE.$interm));
+            $type=array("type"=>"Feature","properties"=>$ligne1,"geometry"=>$ligne2);
+            $tableau[]=$type;
+        }
+        $tableau2[]=array("FeatureCollection"=>$tableau);
+        echo json_encode($tableau2);
+    }
 ?>
