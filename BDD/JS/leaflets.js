a4c14a024fc48751c4ebea545bc9b3fb497166fb
jil3401 <jil3401@gmail.com>
2022-12-01 14:33:36 +0100

    jte laisse des indication sur comment utiliser les BDD zoubi

--- BDD/JS/leaflets.js
+++ BDD/JS/leaflets.js
@@ -6,7 +6,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
 //nous réalisons un fetch de la fonction  
 var oisif=document.getElementById('check1');
 oisif.addEventListener("input", function(){
-	var data = {"data":1};
+	var data = {"data":1};// but ici (si on fait plusieurs fetch c'est de demander au fichier PHP de renvoyer que ce qui est demandée et pas le reste)
 	fetch('../PHP/json_point_request.php', {
 		method: 'post',
 		body: JSON.stringify(data)
@@ -14,14 +14,14 @@ oisif.addEventListener("input", function(){
 		.then(r => r.json())
 		.then(r=> {
 			for (var i=0; i<r.length; i++){
-				var lat = r[i]["lat"];
-				var long =r[i]["long"];
+				var lat = r[i]["lat"];//selection de la colonne lat
+				var long =r[i]["long"];//selection de la colonne long
 				var mark = L.marker([lat, long]).addTo(map);
-				if (r[i]["popup"]!=""){
+				if (r[i]["popup"]!=""){//test de popup pour voir que cela marche
 					mark.bindPopup(r[i]["popup"]).openPopup();
-					console.log(r[i]["popup"]);
+					console.log(r[i]["popup"]);//vérifier que cela marche (j'ai mis un popup à un seul endroit si tu utilises le fichier .sql que je t'ai envoyée (tu peux aussi voir tous les colonnes SQL))
 				}
 				}
 			})
-		.then(r => console.log(r));
+		.then(r => console.log(r));//inutile je pense mais bien pour voir tous les erreurs si jamais le fetch n'est pas bon (bon en général ça vient du PHP)
 	})
--- BDD/PHP/json_point_request.php
+++ BDD/PHP/json_point_request.php
@@ -1,13 +1,12 @@
 <?php
-    include('connect.php');
-    $reveur=json_decode(file_get_contents('php://input'), true);
-    $request1=mysqli_query($link, "SELECT * FROM objet AS ob JOIN point AS po ON ob.id_point=po.id_point");
+    include('connect.php');//connexion à la BDD
+    $reveur=json_decode(file_get_contents('php://input'), true);//pour récupérer le data envoyé et à partir de ça selon les données reçues, on peut choisir quel tableau renvoyer
+    $request1=mysqli_query($link, "SELECT * FROM objet AS ob JOIN point AS po ON ob.id_point=po.id_point");//commande SQL (adaptable) pour reçevoir que l'ID, si nécéssaire, on peut renvoyer des JSON directement si on utilise les bonnes commandes SQL
     if($reveur["data"]=="1"){
         $tableau=[];
-        while($ligne = mysqli_fetch_assoc($request1)){
-            $tableau[]=$ligne;
+        while($ligne = mysqli_fetch_assoc($request1)){//transforme ton résultat SQL, non encodée en ligne de tableau associatif sous forme {"nom_colonne_sql":"colonne i"}, particularité, la première occurence lance la 1ere ligne du tableau SQL, la 2ème commande lance la 2ème ligne du tableau SQL, etc.
+            $tableau[]=$ligne;//on crée le json directement dans le format qu'on veut, ici un tableau 
         }
-        // $tableau2[]=array("FeatureCollection"=>$tableau);
         echo json_encode($tableau);
     }
 ?>
