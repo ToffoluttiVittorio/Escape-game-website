b8653f471d4fc3389c45c359d71d7595a55fbfa2
Vitto50 <vittorio.toffolutti@gmail.com>
2022-12-05 01:31:22 +0100

    bdd avec msg plus clairs

--- bdd_objet.sql
+++ bdd_objet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 05, 2022 at 12:02 AM
+-- Generation Time: Dec 05, 2022 at 12:30 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -45,16 +45,16 @@ CREATE TABLE `objet` (
 --
 
 INSERT INTO `objet` (`id_objet`, `longitude`, `latitude`, `ID_type`, `URL_image`, `Code`, `grid_position`, `popup`, `popupbis`, `objet_inventaire`) VALUES
-(1, 48.888462, 2.29688, 4, '../img/cafe.png', 0, 0, '\"On vous donne un rendez-vous au nord de la ville dans le parc des Chanteraines\"', '0', '0'),
-(2, 48.939153, 2.3116771, 4, '../img/parc.png', 4, 2, '\"On vous donne une promesse de vente et !! On vous appelle pour une affaire urgente à la mairie\"', '0', '../img/parc.png'),
-(3, 48.892684, 2.287328, 4, '../img/mairie.png', 2, 3, '\"Lors d\'une réunion de dernière minute, vous apprenez que madame Desfontaines veut vous faire part d\'un cadeau, accompagnez la à la banque de France\"', '0', '0'),
-(4, 48.865289, 2.340198, 2, '../img/banque.png', 1, 5, '\"Mince ! Elle ne se souvient plus de code d\'entrée, allez chez elle demander le code à son marie, elle habiterai sur l\'île Saint-Louis\" ', '0', ''),
-(5, 48.851557, 2.35692, 3, '../img/code.png', 3, 2, 'Bien joué, maintenant, prenez l\'avion', '\"Le marie vous informe que le code est 2525\"', '0'),
-(6, 49.002945, 2.544352, 0, '../img/avion.png', 21, 21, '\"Retournez à l\'aéroport\"', '\"Aller dans votre maison de vacances à giverny\"', '../img/valise.png'),
+(1, 48.888462, 2.29688, 4, '../img/cafe.png', 0, 0, '\"On vous donne un rendez-vous au nord de la ville dans le parc des Chanteraines. Vous avez entendu qu\'une réunion entre plusieurs promoteurs de la ville s\'y déroulait.\"', '0', '0'),
+(2, 48.939153, 2.3116771, 4, '../img/parc.png', 4, 2, '\"On vous donne une promesse de vente et !! On vous appelle pour une affaire urgente à la mairie. Retournez voir ce qu\'il se trame à la mairie de Levallois-Perret!\"', '0', '../img/parc.png'),
+(3, 48.892684, 2.287328, 4, '../img/mairie.png', 2, 3, '\"Lors d\'une réunion de dernière minute, vous apprenez que madame Desfontaines veut vous faire part d\'un cadeau, accompagnez la à la banque de France. Vous avez encore fait jouer vos relations, vous pouvez être fier de vous!\"', '0', '0'),
+(4, 48.865289, 2.340198, 2, '../img/banque.png', 1, 5, '\"Mince ! Elle ne se souvient plus de code d\'entrée, allez chez elle demander le code à son marie, elle habiterai sur l\'île Saint-Louis, toujours être entouré de personnes fortunées.\"', '0', ''),
+(5, 48.851557, 2.35692, 3, '../img/code.png', 3, 2, '\"Votre femme vous appelle et vous dit de venir en urgence vous rendre à l\'aéroport de Roissy Charles de Gaulle !\"', '\"Le marie de madame Desfontaines vous informe que le code est 2525\"', '0'),
+(6, 49.002945, 2.544352, 0, '../img/avion.png', 21, 21, '\"Retournez à l\'aéroport\"', '\"Arrivée à l\'aéroport, vous vous souvenez que vous avez laissé beaucoup trop de document confidentiels dans votre résidence. Aller dans votre maison de vacances à Giverny au plus vite!\"', '../img/valise.png'),
 (7, 49.073748, 1.539402, 1, '../img/manoir.png', 852, 3, '\"Vous arriver à l\'aéroport de Caen\"', '\"Retournez à l\'aéroport\"', '../img/valise.png'),
 (8, 49.183118, -0.456529, 4, '../img/avion.png', 52, 452, '\"Votre femme vous rappelle, allez dans le centre ville de Caen\"', 'likyujtgreds', '0'),
 (9, 49.181472, -0.370622, 0, '../img/biere.png', 45, 4, '\"Voici votre femme ! Elle vous a fait une petite surprise et vous emmène à l’hôtel\"', '\"Vous oubliez de lui prendre des fleurs! Allez lui cueillir des fleurs dans le parc du golf,au Nord de la ville, vous en profiterez en même temps pour peaufinez votre swing.\"', '../img/fleurs.png'),
-(10, 49.227959, -0.336221, 1, '../img/golf.png', 5, 4, '\"Voici votre femme ! Elle vous a fait une petite surprise et vous emmène à l’hôtel\"', '\"Après cette petite promenade de santé, et une petite partie de golf, retournez voir votre femme.\"', '../img/fleurs.png');
+(10, 49.227959, -0.336221, 1, '../img/golf.png', 5, 4, '\"Voici votre femme ! Elle vous a fait une petite surprise et vous emmène à l’hôtel\"', '\"Après cette petite promenade de santé, et une petite partie de golf, retournez voir votre femme au centre ville.\"', '../img/fleurs.png');
 
 --
 -- Indexes for dumped tables
